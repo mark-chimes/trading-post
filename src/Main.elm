4fe8913227c90993451592cfc1a99592343fd301
Mark Chimes <chimes.mark@gmail.com>
2020-01-19 18:39:19 +0200

    Add pc Gold and minimum of 0 offer

--- src/Main.elm
+++ src/Main.elm
@@ -15,13 +15,15 @@ import String
 type alias Model =
     { pcOfferString : String
     , pcOfferInt : Int
+    , pcGold : Int
+    , itemWorth : Int
     , conversation : List String
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { pcOfferString = "0", pcOfferInt = 0, conversation = [] }, Cmd.none )
+    ( { pcOfferString = "0", pcOfferInt = 0, pcGold = 0, itemWorth = 20, conversation = [] }, Cmd.none )
 
 
 
@@ -42,13 +44,23 @@ update msg model =
             ( model, Cmd.none )
 
         PcOffer newOffer ->
-            ( { model | pcOfferString = newOffer, pcOfferInt = Maybe.withDefault model.pcOfferInt (String.toInt newOffer) }, Cmd.none )
+            ( { model | pcOfferString = newOffer, pcOfferInt = max 0 (Maybe.withDefault model.pcOfferInt (String.toInt newOffer)) }, Cmd.none )
 
         ModifyPcOffer amount ->
-            ( { model | pcOfferString = String.fromInt (model.pcOfferInt + amount), pcOfferInt = model.pcOfferInt + amount }, Cmd.none )
+            ( { model | pcOfferString = String.fromInt (max 0 (model.pcOfferInt + amount)), pcOfferInt = max 0 (model.pcOfferInt + amount) }, Cmd.none )
 
         SubmitOffer ->
-            ( { model | conversation = model.conversation ++ [ "You offered: " ++ String.fromInt model.pcOfferInt ] }, Cmd.none )
+            ( sellItem <| updateConvoWithOffer <| model, Cmd.none )
+
+
+updateConvoWithOffer : Model -> Model
+updateConvoWithOffer model =
+    { model | conversation = model.conversation ++ [ "You offered: " ++ String.fromInt model.pcOfferInt ] }
+
+
+sellItem : Model -> Model
+sellItem model =
+    { model | pcGold = model.pcGold + model.pcOfferInt }
 
 
 
@@ -59,11 +71,13 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App still working!" ]
+        , h1 [] [ text "Your Elm App is still working!" ]
+        , text ("Your gold: " ++ String.fromInt model.pcGold)
+        , br [] []
         , div []
             [ button [ onClick (ModifyPcOffer -100) ] [ text "-100" ]
             , button [ onClick (ModifyPcOffer -10) ] [ text "-10" ]
-            , input [ Attr.type_ "number", Attr.min "-50000", Attr.max "50000", placeholder "Your Offer", value model.pcOfferString, onInput PcOffer ] []
+            , input [ Attr.type_ "number", Attr.min "0", Attr.max "50000", placeholder "Your Offer", value model.pcOfferString, onInput PcOffer ] []
             , button [ onClick (ModifyPcOffer 10) ] [ text "+10" ]
             , button [ onClick (ModifyPcOffer 100) ] [ text "+100" ]
             ]

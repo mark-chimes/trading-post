81437b66fb9a8e831d03c32ea53a00b680ea1ee9
Mark Chimes <chimes.mark@gmail.com>
2020-06-07 18:10:03 +0200

    Grid gaps for conversation.

--- src/Main.elm
+++ src/Main.elm
@@ -1404,6 +1404,8 @@ convertMessageListToGrid listOfPairs =
             , div [ Attr.class "convoEmpty" ] []
             , div [ Attr.class "convoEmpty" ] []
             , div [ Attr.class "convo-right " ] [ text y ]
+            , div [ Attr.class "convoEmpty" ] []
+            , div [ Attr.class "convoEmpty" ] []
             ]
         )
         listOfPairs
